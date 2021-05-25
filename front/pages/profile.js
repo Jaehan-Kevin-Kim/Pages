be914463ef0ec544bb031c65dfc59d2f879b5f7a
Kevin Kim <jhkevin21@gmail.com>
2021-05-25 00:46:56 -0600

    Created s dummy data for posts  using faker API.

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,5 +1,6 @@
+import Router from "next/router";
 import Head from "next/head";
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import AppLayout from "../components/AppLayout";
 import FollowerList from "../components/FollowerList";
@@ -10,6 +11,15 @@ const profile = () => {
   // const followerList = [{ nickname: "kevin" }, { nickname: "sample" }, { nickname: "admin user" }];
   // const followingList = [{ nickname: "kevin" }, { nickname: "sample" }, { nickname: "admin" }];
   const { me } = useSelector((state) => state.user);
+  useEffect(() => {
+    if (!(me && me.id)) {
+      Router.push("/");
+    }
+  }, [me && me.id]);
+
+  if (!me) {
+    return null;
+  }
   return (
     <>
       <Head>
