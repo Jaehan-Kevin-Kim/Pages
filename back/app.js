1cb2d06c0e66f36ce6a608cbbafeaf04bed006cc
Kevin Kim <jhkevin21@gmail.com>
2021-05-29 15:09:48 -0600

    Created back-end server, setting express and routes

---
+++ back/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const postRouter = require('./routes/post');
+
+app.get('/', (req, res) => {
+  res.send('hello express');
+});
+app.get('/api', (req, res) => {
+  res.send('hello api');
+});
+app.get('/posts', (req, res) => {
+  res.json([
+    { id: 1, content: 'hello' },
+    { id: 2, content: 'hello2' },
+    { id: 3, content: 'hello3' },
+  ]);
+});
+
+app.use('/post', postRouter);
+
+app.listen(3065, () => {
+  console.log('Server is running');
+});
