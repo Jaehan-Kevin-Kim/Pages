77162d55c7eb36769783e0685f72f45f9bbd6fa5
Kevin Kim <jhkevin21@gmail.com>
2021-08-05 22:48:50 -0600

    Update localhost address to backend server's address

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -15,6 +15,7 @@ import {
 import wrapper from "../store/configureStore";
 import axios from "axios";
 import { END } from "redux-saga";
+import { backUrl } from "../config/config";
 
 const fetcher = (url) => axios.get(url, { withCredentials: true }).then((result) => result.data);
 
@@ -27,11 +28,11 @@ const profile = () => {
   const [followingsLimit, setFollowingsLimit] = useState(3);
 
   const { data: followersData, error: followerError } = useSWR(
-    `http://localhost:3065/user/followers?limit=${followersLimit}`,
+    `${backUrl}/user/followers?limit=${followersLimit}`,
     fetcher
   );
   const { data: followingsData, error: followingError } = useSWR(
-    `http://localhost:3065/user/followings?limit=${followingsLimit}`,
+    `${backUrl}/user/followings?limit=${followingsLimit}`,
     fetcher
   );
 
